4227fd1ce264ce71241cadf9624f4b7493b933a7
Mutagah <kihahumutagah@gmail.com>
2024-01-16 17:26:56 +0300

    :art:Creation of header component to contain all header sub-component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 @Component({
-  selector: 'app-header',
+  selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
- faMagnifyingGlass = faMagnifyingGlass;
- faUser = faUser;
- faCartShopping = faCartShopping;
+
 }

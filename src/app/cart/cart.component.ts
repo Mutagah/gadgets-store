d8cb3bf2a9716e38a346088ee78dbf1a10861957
Mutagah <kihahumutagah@gmail.com>
2024-01-24 13:05:06 +0300

    :lipstick::construction:feat: Cart UI update.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+  faCircleArrowLeft = faCircleArrowLeft;
+  constructor(private router: Router){}
+  navigateToShop(){
+    this.router.navigate(['/shop']);
+  }
 }
